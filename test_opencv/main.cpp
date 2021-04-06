21af3a53abe11d749420aa6a93b540a237a017bd
zhongzisha <zhongzisha@outlook.com>
2021-04-06 22:11:31 +0800

    still debug

--- test_opencv/main.cpp
+++ test_opencv/main.cpp
@@ -4,6 +4,35 @@ using namespace std;
 
 #include <opencv2/opencv.hpp>
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <npp.h>
+#include <nppcore.h>
+#include <nppdefs.h>
+#include <nppi.h>
+#include <nppi_arithmetic_and_logical_operations.h>
+#include <nppi_color_conversion.h>
+#include <nppi_data_exchange_and_initialization.h>
+#include <nppi_filtering_functions.h>
+#include <nppi_geometry_transforms.h>
+#include <nppi_linear_transforms.h>
+#include <nppi_morphological_operations.h>
+#include <nppi_statistics_functions.h>
+#include <nppi_support_functions.h>
+#include <nppi_threshold_and_compare_operations.h>
+#include <npps.h>
+#include <npps_arithmetic_and_logical_operations.h>
+#include <npps_conversion_functions.h>
+#include <npps_filtering_functions.h>
+#include <npps_initialization.h>
+#include <npps_statistics_functions.h>
+#include <npps_support_functions.h>
+
+#include <stdio.h>
+
+extern "C" cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
 ///
 /// \brief 测试opencv的mat的内存布局，详情见图片
 /// \return
@@ -139,8 +168,8 @@ void dark_channel_prior_defog(const std::string& filename) {
         for(int x=0; x<width;x++) {
             jx.at<cv::Vec3f>(y,x) = cv::Vec3f(
                         (src_n.at<cv::Vec3f>(y,x)[0]-A[0])/std::max(tx.at<float>(y,x),t0)+A[0],
-                        (src_n.at<cv::Vec3f>(y,x)[1]-A[1])/std::max(tx.at<float>(y,x),t0)+A[1],
-                        (src_n.at<cv::Vec3f>(y,x)[2]-A[2])/std::max(tx.at<float>(y,x),t0)+A[2]);
+                    (src_n.at<cv::Vec3f>(y,x)[1]-A[1])/std::max(tx.at<float>(y,x),t0)+A[1],
+                    (src_n.at<cv::Vec3f>(y,x)[2]-A[2])/std::max(tx.at<float>(y,x),t0)+A[2]);
         }
     }
 
@@ -252,8 +281,8 @@ void dark_channel_prior_defog_for_polar() {
         for(int x=0; x<width;x++) {
             jx.at<cv::Vec3f>(y,x) = cv::Vec3f(
                         (src_n.at<cv::Vec3f>(y,x)[0]-A[0])/std::max(tx.at<float>(y,x),t0)+A[0],
-                        (src_n.at<cv::Vec3f>(y,x)[1]-A[1])/std::max(tx.at<float>(y,x),t0)+A[1],
-                        (src_n.at<cv::Vec3f>(y,x)[2]-A[2])/std::max(tx.at<float>(y,x),t0)+A[2]);
+                    (src_n.at<cv::Vec3f>(y,x)[1]-A[1])/std::max(tx.at<float>(y,x),t0)+A[1],
+                    (src_n.at<cv::Vec3f>(y,x)[2]-A[2])/std::max(tx.at<float>(y,x),t0)+A[2]);
         }
     }
 
@@ -270,11 +299,201 @@ void dark_channel_prior_defog_for_polar() {
 }
 
 
-
-int main(int argc, char** argv) {
+int test_cuda_add(int argc, char** argv) {
     // std::string filename = std::string(argv[1]);
     // dark_channel_prior_defog(filename);
-    dark_channel_prior_defog_for_polar();
+    // dark_channel_prior_defog_for_polar();
+
+    const int arraySize = 5;
+    const int a[arraySize] = { 1, 2, 3, 4, 5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+           c[0], c[1], c[2], c[3], c[4]);
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
+int main1()
+{
+    string filename = "F:\\wy\\ImageStackAlignator\\pictures\\IMGP3003.PEF";
+    FILE *in = fopen(filename.c_str(), "rb");
+    fseek(in, 0, SEEK_END);
+    unsigned fsz = ftell(in);
+    unsigned char *buffer = (unsigned char *)malloc(fsz);
+    if (!buffer)
+        return 2;
+
+    cout << fsz << endl;
+
+    fseek(in, 0, SEEK_SET);
+    unsigned readb = fread(buffer, 1, fsz, in);
+    if (readb != fsz)
+        return 3;
+
+
+    if(buffer)
+        free(buffer);
+
+    return 0;
+}
+
+vector<float> gaussin_filter_1D(float sigma) {
+    if (sigma <= 0) {
+        return vector<float>({0, 0,0,0,1,0,0,0,0});
+    }
+    int size = (int)(sigma /0.6f - 0.4f) * 2 + 1 + 2;
+    size = min(size, 99);
+    vector<float> ret(size);
+
+    int center = size / 2;
+    for(int i=0;i<size;i++) {
+        int x = i -center;
+        ret[i] = (float)(exp(-(x*x)/(2*sigma*sigma)));
+    }
+    float sum=0;
+    for(int i =0;i<size;i++) {
+        sum+=ret[i];
+    }
+    for(int i=0; i<size;i++) {
+        ret[i] /=sum;
+    }
+    return ret;
+}
+
+void test_npp_rotate() {
+
+    //读取图片
+    string filename = "../subimg0000.png";
+
+    cv::Mat im = cv::imread(filename, cv::IMREAD_ANYCOLOR | cv::IMREAD_ANYDEPTH);
+
+    int height = im.rows;
+    int width = im.cols;
+    int nchannels = im.channels();
+    int step = im.step;  // width * channels = 1024 * 3 = 3072
+    cout << "step = " << step << endl;
+
+    // 这里表明opencv的mat内存布局
+    uchar *data = im.data;
+
+    unsigned char* pSrcData = new unsigned char[height * width * nchannels];
+    unsigned char* pDstData = nullptr;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < nchannels; k++) {
+                pSrcData[i * width * nchannels + j * nchannels + k] =
+                        static_cast<int>(data[i*width*nchannels + j*nchannels + k]);
+                        // static_cast<unsigned char>((static_cast<double>(rand())/ RAND_MAX) * 255);
+            }
+        }
+    }
+
+    cudaError_t cuRet;
+    NppStatus nppRet;
+    Npp8u *pSrcDataDevice = nullptr;
+    Npp8u *pDstDataDevice = nullptr;
+
+    NppiSize oSrcSize = { 0 };
+    NppiSize oDstSize = { 0 };
+    NppiRect oSrcROI = { 0 };
+    NppiRect oDstROI = { 0 };
+    int nImgBpp = 0;
+    int nSrcPitch = 0;
+    int nDstPitch = 0;
+    int nSrcPitchDevice = 0;
+    int nDstPitchDevice = 0;
+    double aBoundingBox[2][2] = { 0 };
+    double nAngle = 0;
+
+    nImgBpp = nchannels;
+    oSrcSize.width = width;
+    oSrcSize.height = height;
+    nSrcPitch = width * nchannels;
+
+    nAngle = atof("90");
+
+    oSrcROI.x = oSrcROI.y = 0;
+    oSrcROI.width = oSrcSize.width;
+    oSrcROI.height = oSrcSize.height;
+
+    // 分配显存
+    pSrcDataDevice = nppiMalloc_8u_C3(oSrcSize.width * nImgBpp, oSrcSize.height, &nSrcPitchDevice);
+    cudaMemcpy2D(pSrcDataDevice, nSrcPitchDevice, pSrcData, nSrcPitch,
+                 oSrcSize.width * nImgBpp, oSrcSize.height, cudaMemcpyHostToDevice);
+
+    nppiGetRotateBound(oSrcROI, aBoundingBox, nAngle, 0, 0);
+    oDstSize.width = static_cast<int>(ceil(fabs(aBoundingBox[1][0] - aBoundingBox[0][0])));
+    oDstSize.height = static_cast<int>(ceil(fabs(aBoundingBox[1][1] - aBoundingBox[0][1])));
+
+    pDstData = new unsigned char[oDstSize.height * oDstSize.width * nchannels];
+
+    nDstPitch = oDstSize.width * nchannels;
+    oDstROI.x = oDstROI.y = 0;
+    oDstROI.width = oDstSize.width;
+    oDstROI.height = oDstSize.height;
+
+    pDstDataDevice = nppiMalloc_8u_C3(oDstSize.width, oDstSize.height, &nDstPitchDevice);
+    cudaMemset2D(pDstDataDevice, nDstPitchDevice, 0, oDstSize.width * nDstPitch, oDstSize.height);
+    nppRet = nppiRotate_8u_C3R(pSrcDataDevice, oSrcSize, nSrcPitchDevice, oSrcROI,
+                               pDstDataDevice, nDstPitchDevice, oDstROI,
+                               nAngle, -aBoundingBox[0][0], -aBoundingBox[0][1], NPPI_INTER_CUBIC);
+    cudaMemcpy2D(pDstData, nDstPitch, pDstDataDevice, nDstPitchDevice, oDstSize.width * nImgBpp, oDstSize.height, cudaMemcpyDeviceToHost);
+
+//    for (int i = 0; i < oDstSize.height; i++) {
+//        for (int j = 0; j < oDstSize.width; j++) {
+//            std::cout << i << ", " << j << ": ";
+//            for (int k = 0; k < nchannels; k++) {
+//                std::cout << static_cast<int>(pSrcData[i * oDstSize.width * nchannels + j * nchannels + k]) << ", ";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+
+    // 这里先分配一个图像，保存结果，然后直接用内存复制的方法将结果复制到图片中，然后保存
+    cv::Mat dst = cv::Mat::zeros(oDstSize.height, oDstSize.width, CV_8UC3);
+    memcpy(dst.data, pDstData, oDstSize.height * oDstSize.width * nchannels );
+    cv::imwrite("dst.png", dst);
+
+    delete[] pSrcData;
+    delete[] pDstData;
+
+    nppiFree(pSrcDataDevice);
+    nppiFree(pDstDataDevice);
+
+    cudaDeviceReset();
+}
+
+int main(){
+    float SigmaDebayerTracking =0.5f;
+    vector<float> filter = gaussin_filter_1D(SigmaDebayerTracking);
+
+    for (auto value:filter) {
+        cout << value << endl;
+    }
+
+    test_npp_rotate();
+
+    return 0;
 }
 
 
--- test_opencv/test_opencv.pro
+++ test_opencv/test_opencv.pro
@@ -8,8 +8,60 @@ SOURCES += \
 
 
 INCLUDEPATH += "F:/opencv-4.5.1/build_release/install/include"
+INCLUDEPATH += "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/include"
 LIBS += "F:/opencv-4.5.1/build_release/install/x64/vc16/lib/opencv_core451.lib"
 LIBS += "F:/opencv-4.5.1/build_release/install/x64/vc16/lib/opencv_imgcodecs451.lib"
 LIBS += "F:/opencv-4.5.1/build_release/install/x64/vc16/lib/opencv_highgui451.lib"
 LIBS += "F:/opencv-4.5.1/build_release/install/x64/vc16/lib/opencv_imgproc451.lib"
 LIBS += "F:/opencv-4.5.1/build_release/install/x64/vc16/lib/opencv_world451.lib"
+
+## This makes the .cu files appear in your project
+#CUDA_SOURCES +=  \
+#DeBayerKernels.cu \
+#RobustnessModell.cu \
+#ShiftMinimizerKernels.cu \
+#kernel.cu \
+#opticalFlow.cu
+
+## Project dir and outputs
+#OBJECTS_DIR = $$PWD
+#DESTDIR = $$PWD
+
+## Path to cuda toolkit install
+#CUDA_DIR = "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2"
+## GPU architecture
+#CUDA_ARCH = sm_75
+## nvcc flags (ptxas option verbose is always useful)
+#NVCCFLAGS = --compiler-options -use-fast-math --Wno-deprecated-gpu-targets
+## include paths
+#INCLUDEPATH += $$CUDA_DIR/include
+## lib dirs
+#QMAKE_LIBDIR += $$CUDA_DIR/lib/x64
+## libs - note than i'm using a x_86_64 machine
+#LIBS += -lcuda -lcudart
+## join the includes in a line
+#CUDA_INC = $$join(INCLUDEPATH,' -I','-I',' ')
+
+#cuda.input = CUDA_SOURCES
+#cuda.output = ${OBJECTS_DIR}/${QMAKE_FILE_BASE}_cuda.o
+#cuda.commands = $$CUDA_DIR/bin/nvcc -m64 -g -G -arch=$$CUDA_ARCH -c $$NVCCFLAGS $$CUDA_INC $$LIBS ${QMAKE_FILE_NAME} -o ${QMAKE_FILE_OUT}
+#cuda.dependency_type = TYPE_C
+#cuda.depend_command = $$CUDA_DIR/bin/nvcc -g -G -M $$CUDA_INC $$NVCCFLAGS ${QMAKE_FILE_NAME}
+## Tell Qt that we want add more stuff to the Makefile
+#QMAKE_EXTRA_UNIX_COMPILERS += cuda
+
+
+# This makes the .cu files appear in your project
+OTHER_FILES +=  \
+DeBayerKernels.cu \
+RobustnessModell.cu \
+ShiftMinimizerKernels.cu \
+kernel.cu \
+opticalFlow.cu
+
+DISTFILES += \
+    build.bat
+
+
+
+
